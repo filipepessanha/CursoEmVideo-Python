5396fe8641f957d969cf96f655938326aff6df10
filipepessanha <68128627+filipepessanha@users.noreply.github.com>
2020-07-13 17:08:12 -0300

    Alteração no módulo e importação.
    Foi colocado o arquivo 'moeda' dentro da pasta mod e, consequentemente, alterado a chamada do módulo no programa.

--- Exercícios/ex107/workspace.py
+++ Exercícios/ex107/workspace.py
@@ -1,7 +1,7 @@
-import moeda
+from mod.moeda import *
 
 p = float(input('\033[32mDigite o preço: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} é {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
-print(f'Reduzindo 13%, temos {moeda.reduzir(p, 13)}')
+print(f'A metade de {p} é {metade(p)}')
+print(f'O dobro de {p} é {dobro(p)}')
+print(f'Aumentando 10%, temos {aumentar(p, 10)}')
+print(f'Reduzindo 13%, temos {reduzir(p, 13)}')
